19f72987e2a870c49c69d130172295b8b1f6facf
Adwait <adwait.kul.2018@gmail.com>
2022-12-16 13:11:32 -0800

    Added check for blackjack condition

--- main.py
+++ main.py
@@ -54,10 +54,13 @@ user_cards.append(second_user_card)
 user_cards.append(first_computer_card)
 user_cards.append(second_computer_card)
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
+def calculate_score(cards):
+    if (sum(cards) == 21 and len(cards) == 2):
+        return 0
+    elif ():
+        return 0
+    return sum(cards)
+        
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
 #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
