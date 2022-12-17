a205d8ad8d0046c1ef798ea460de7af7ab3ebc1f
Adwait <adwait.kul.2018@gmail.com>
2022-12-16 16:09:43 -0800

    Need to make this work

--- main.py
+++ main.py
@@ -11,28 +11,13 @@
 # The computer is the dealer.
 
 import random
+import os
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
     random_number = random.randrange(len(cards))
     return cards[random_number]
-
-user_cards = []
-computer_cards = []
-
-first_user_card = deal_card()
-second_user_card = deal_card()
-
-first_computer_card = deal_card()
-second_computer_card = deal_card()
-
-user_cards.append(first_user_card)
-user_cards.append(second_user_card)
-
-user_cards.append(first_computer_card)
-user_cards.append(second_computer_card)
-
 
 def calculate_score(cards):
     if (sum(cards) == 21 and len(cards) == 2):
@@ -42,28 +27,43 @@ def calculate_score(cards):
         cards.append(1)
     return sum(cards)
 
+def play_game():
+    is_game_over = False
 
-is_game_over = False
+    while not is_game_over:
+        user_cards = []
+        computer_cards = []
 
-while not is_game_over:
-    sum_of_user_cards = calculate_score(user_cards)
-    sum_of_computer_cards = calculate_score(computer_cards)
-    
-    print(f"Your cards are: {user_cards}, your score is {sum_of_user_cards}")
-    print(f"The computer cards are: {computer_cards}, computer's score is {sum_of_computer_cards}")
-    
-    if (sum_of_user_cards == 0 or sum_of_computer_cards == 0 or sum_of_user_cards > 21):
-        is_game_over = True
-    else:
-        user_input = input("Do you want to draw another card? (y/n): ")
-        if user_input == "y":
-            user_cards.append(deal_card())
-        else:
+        first_user_card = deal_card()
+        second_user_card = deal_card()
+
+        first_computer_card = deal_card()
+        second_computer_card = deal_card()
+
+        user_cards.append(first_user_card)
+        user_cards.append(second_user_card)
+
+        user_cards.append(first_computer_card)
+        user_cards.append(second_computer_card)
+
+        sum_of_user_cards = calculate_score(user_cards)
+        sum_of_computer_cards = calculate_score(computer_cards)
+
+        print(f"Your cards are: {user_cards}, your score is {sum_of_user_cards}")
+        print(f"The computer cards are: {computer_cards}, computer's score is {sum_of_computer_cards}")
+
+        if (sum_of_user_cards == 0 or sum_of_computer_cards == 0 or sum_of_user_cards > 21):
             is_game_over = True
+        else:
+            user_input = input("Do you want to draw another card? (y/n): ")
+            if user_input == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
 
-while sum_of_computer_cards < 17:
-    computer_cards.append(deal_card())
-    sum_of_computer_cards = calculate_score(computer_cards)
+    while sum_of_computer_cards < 17:
+        computer_cards.append(deal_card())
+        sum_of_computer_cards = calculate_score(computer_cards)
 
 def compare(sum_of_user_cards, sum_of_computer_cards):
     if sum_of_user_cards == sum_of_computer_cards:
@@ -82,3 +82,7 @@ def compare(sum_of_user_cards, sum_of_computer_cards):
         return "You lose"        
 
 # Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+start_game_user_input = input("Do you want to restart the game? (y/n): ")
+if (start_game_user_input == "y"):
+    os.system('CLS')
+    play_game()
