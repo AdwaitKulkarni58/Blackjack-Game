bdafbed90a6f8e442e874e7b513f59176be72d36
Adwait <adwait.kul.2018@gmail.com>
2022-12-16 16:38:20 -0800

    Finally got it to work

--- main.py
+++ main.py
@@ -11,11 +11,9 @@
 # The computer is the dealer.
 
 import random
-import os
-
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     random_number = random.randrange(len(cards))
     return cards[random_number]
 
@@ -27,46 +25,10 @@ def calculate_score(cards):
         cards.append(1)
     return sum(cards)
 
-def play_game():
-    is_game_over = False
-
-    while not is_game_over:
-        user_cards = []
-        computer_cards = []
-
-        first_user_card = deal_card()
-        second_user_card = deal_card()
-
-        first_computer_card = deal_card()
-        second_computer_card = deal_card()
-
-        user_cards.append(first_user_card)
-        user_cards.append(second_user_card)
-
-        user_cards.append(first_computer_card)
-        user_cards.append(second_computer_card)
-
-        sum_of_user_cards = calculate_score(user_cards)
-        sum_of_computer_cards = calculate_score(computer_cards)
-
-        print(f"Your cards are: {user_cards}, your score is {sum_of_user_cards}")
-        print(f"The computer cards are: {computer_cards}, computer's score is {sum_of_computer_cards}")
-
-        if (sum_of_user_cards == 0 or sum_of_computer_cards == 0 or sum_of_user_cards > 21):
-            is_game_over = True
-        else:
-            user_input = input("Do you want to draw another card? (y/n): ")
-            if user_input == "y":
-                user_cards.append(deal_card())
-            else:
-                is_game_over = True
-
-    while sum_of_computer_cards < 17:
-        computer_cards.append(deal_card())
-        sum_of_computer_cards = calculate_score(computer_cards)
-
 def compare(sum_of_user_cards, sum_of_computer_cards):
-    if sum_of_user_cards == sum_of_computer_cards:
+    if sum_of_user_cards > 21 and sum_of_computer_cards > 21:
+        return "Total more than 21, you lose"
+    elif sum_of_user_cards == sum_of_computer_cards:
         return "It is a draw"
     elif sum_of_computer_cards == 0:
         return "You lose"
@@ -79,10 +41,53 @@ def compare(sum_of_user_cards, sum_of_computer_cards):
     elif sum_of_user_cards > sum_of_computer_cards:
         return "You win"
     else:
-        return "You lose"        
+        return "You lose" 
 
-# Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
-start_game_user_input = input("Do you want to restart the game? (y/n): ")
+def play_game():
+    is_game_over = False
+    
+    user_cards = []
+    computer_cards = [] 
+    
+    first_user_card = deal_card()
+    second_user_card = deal_card()
+    
+    first_computer_card = deal_card()
+    second_computer_card = deal_card()
+    
+    user_cards.append(first_user_card)
+    user_cards.append(second_user_card)
+    
+    computer_cards.append(first_computer_card)
+    computer_cards.append(second_computer_card)
+
+    while not is_game_over:
+
+        sum_of_user_cards = calculate_score(user_cards)
+        print(f"Your cards are: {user_cards}, your score is {sum_of_user_cards}")
+        
+        sum_of_computer_cards = calculate_score(computer_cards)
+        print(f"The computer's first card is: {computer_cards[0]}")
+
+        if (sum_of_user_cards == 0 or sum_of_computer_cards == 0 or sum_of_user_cards > 21):
+            is_game_over = True
+            
+        elif input("Do you want to draw another card? (y/n): ") == "y":
+            user_cards.append(deal_card())
+            return
+            
+        is_game_over = True
+            
+
+    while sum_of_computer_cards < 17:
+        computer_cards.append(deal_card())
+        sum_of_computer_cards = calculate_score(computer_cards)
+        
+    print(f"Your final cards were: {user_cards}, your final score was: {sum_of_user_cards}")
+    print(f"Computer's final cards were: {computer_cards}, computer's final score was: {sum_of_computer_cards}")
+        
+    print(compare(sum_of_user_cards, sum_of_computer_cards))       
+
+start_game_user_input = input("Do you want to start a new game? (y/n): ")
 if (start_game_user_input == "y"):
-    os.system('CLS')
     play_game()
